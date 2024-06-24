2e63b1e327423d8d1935738f0509caae001f8d45
Daniel <McsColosPlay@gmail.com>
2024-06-25 00:13:34 +0300

    fix problem issue

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,4 +1,4 @@
-import { UseDispatch, useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import type { AppDispatch, RootState } from "./store";
 
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
